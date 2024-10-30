796ce0868e5b35da797547f5b82a690485db427e
Jorge Luis <jorgeluis199454@gmail.com>
2024-10-30 09:47:01 -0500

    Create model user

---
+++ Backend_User/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_User.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }= "";
+        public string Email { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
